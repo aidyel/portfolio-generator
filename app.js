510dda99372053fd5c44d3b74e6b36506f3b7eb8
aidyel <aidayeli@icloud.com>
2022-03-17 17:45:06 -0400

    modified HTML template to handle more data

--- app.js
+++ app.js
@@ -10,6 +10,7 @@
 // };
 
 // printProfileData(profileDataArgs);
+
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
@@ -31,7 +32,6 @@ return inquirer.prompt([
             }
         },
         {
-            type: 'input',
             name: 'github',
             message: 'Enter your Github Username (Required)',
             validate: usernameInput => {
@@ -155,13 +155,13 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
-           // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
     });
  
 
