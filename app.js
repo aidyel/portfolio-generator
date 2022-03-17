8130dd3700ac639ae52a79491f8393a702036b55
aidyel <aidayeli@icloud.com>
2022-03-17 12:38:10 -0400

    npm documentation

--- app.js
+++ app.js
@@ -10,22 +10,27 @@
 // };
 
 // printProfileData(profileDataArgs);
-const fs = require('fs');
-const { compileFunction } = require('vm');
-
-const generatePage = require('./src/page-template.js');
-
-const profileDataArgs = process.argv.slice(2);
-console.log(profileDataArgs);
-
-const[iname, github] = profileDataArgs;
-console.log(iname, github);
-
-const pageHTML = generatePage(iname, github);
 
 
-fs.writeFile('index.html', pageHTML, err => {
-    if (err) throw err;
+const inquirer = require('inquirer');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
+
+
+// const pageHTML = generatePage(iname, github);
+
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
     
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
