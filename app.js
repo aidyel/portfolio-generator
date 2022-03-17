6eb1b88a9e12b008adf3b8c7a21f903d92a0f970
aidyel <aidayeli@icloud.com>
2022-03-17 09:57:29 -0400

    Generate HTML File

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for(let i = 0; i <profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
 
-const printProfileData = profileDataArr => {
-    // This...
-    for(let i = 0; i <profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
+const[iname, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(iname, github), err => {
+    if (err) throw err;
+    
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+})
